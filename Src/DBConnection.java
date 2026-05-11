93adfec78dcf3df854a4617267a074b0c7a3d5ce
Anubarathi M <anubarathimanikandan@gmail.com>
2026-05-11 21:31:06 +0530

    Update DBConnection.java

--- Src/DBConnection.java
+++ Src/DBConnection.java
@@ -1,1 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBConnection {
+
+    static String url = "jdbc:mysql://localhost:3306/helpdesk_ticket";
+    static String username = "root";
+    static String password = "your_password";
+
+    public static Connection getConnection() {
+
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return null;
+    }
+}

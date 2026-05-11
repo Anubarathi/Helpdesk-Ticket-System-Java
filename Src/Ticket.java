21aa3c8b3d50c2a15fde5ba840a38cd0deffa2ca
Anubarathi M <anubarathimanikandan@gmail.com>
2026-05-11 21:31:25 +0530

    Update Ticket.java

--- Src/Ticket.java
+++ Src/Ticket.java
@@ -1,1 +1,20 @@
+public class Ticket {
 
+    int id;
+    String customerName;
+    String issueDescription;
+    String status;
+
+    public Ticket(int id, String customerName, String issueDescription, String status) {
+        this.id = id;
+        this.customerName = customerName;
+        this.issueDescription = issueDescription;
+        this.status = status;
+    }
+
+    public Ticket(String customerName, String issueDescription, String status) {
+        this.customerName = customerName;
+        this.issueDescription = issueDescription;
+        this.status = status;
+    }
+}
